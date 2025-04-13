4e224616d0a67ce570b4f33b154b53cc39eb4ff8
bantee-sharma <banteesharma9@gmail.com>
2025-04-13 21:27:15 +0530

    Update mmr.py

--- mmr.py
+++ mmr.py
@@ -9,7 +9,7 @@ docs = [
     Document(page_content="Chroma is used to store and search document embeddings."),
     Document(page_content="Embeddings are vector representations of text."),
     Document(page_content="MMR helps you get diverse results when doing similarity search."),
-    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more."),
+    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more.")
 ]
 
 embeddings = HuggingFaceEmbeddings()
@@ -19,7 +19,7 @@ db = FAISS.from_documents(
     embedding=embeddings,
     
 )
-
+ 
 retriever = db.as_retriever(
     search_type="mmr",
     search_kwargs = {"k":3, "lambda_mult":0.5}
